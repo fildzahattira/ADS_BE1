0ae61d64e9aae44b8a43285838e78a68fe4ff84e
fildzahattira <fildzahattira560@gmail.com>
2024-01-05 21:30:08 +0700

    optional sort by name

--- app/controllers/karyawan.controller.js
+++ app/controllers/karyawan.controller.js
@@ -3,7 +3,6 @@ const db = require("../models");
 const karyawanFromDb = db.karyawans;
 const cutiFromDb = db.cutis;
 
-
 //Mendapatkan seluruh data karyawan
 exports.findAll = (req, res) => {
   karyawanFromDb
@@ -59,72 +58,97 @@ exports.create = (req, res) => {
 
 //Update data karyawan
 exports.update = (req, res) => {
-    const Nomor_Induk = req.params.Nomor_Induk;
-    karyawanFromDb
+  const Nomor_Induk = req.params.Nomor_Induk;
+  karyawanFromDb
     .update(req.body, {
       where: { Nomor_Induk: Nomor_Induk },
     })
-      .then((num) => {
-        if (num == 1) {
-          res.json({
-            message: "Karyawan updated successfully.",
-            data: req.body,
-          });
-        } else {
-          res.json({
-            message: `Cannot update karyawan with Nomor_Induk=${Nomor_Induk}. Maybe karyawan was not found or req.body is empty!`,
-            data: req.body,
-          });
-        }
-      })
-      .catch((err) => {
-        res.status(500).json({
-          message: err.message || "Some error occurred while updating the karyawan.",
-          data: null,
+    .then((num) => {
+      if (num == 1) {
+        res.json({
+          message: "Karyawan updated successfully.",
+          data: req.body,
         });
+      } else {
+        res.json({
+          message: `Cannot update karyawan with Nomor_Induk=${Nomor_Induk}. Maybe karyawan was not found or req.body is empty!`,
+          data: req.body,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message:
+          err.message || "Some error occurred while updating the karyawan.",
+        data: null,
       });
-  };
+    });
+};
 
 //Delete data karyawan
 exports.delete = (req, res) => {
-    const Nomor_Induk = req.params.Nomor_Induk;
-    karyawanFromDb
+  const Nomor_Induk = req.params.Nomor_Induk;
+  karyawanFromDb
     .destroy({
       where: { Nomor_Induk: Nomor_Induk },
     })
-      .then((num) => {
-        if (num == 1) {
-          res.json({
-            message: "Karyawan deleted successfully.",
-            data: req.body,
-          });
-        } else {
-          res.json({
-            message: `Cannot delete karyawan with Nomor_Induk=${Nomor_Induk}. Maybe karyawan was not found!`,
-            data: req.body,
-          });
-        }
-      })
-      .catch((err) => {
-        res.status(500).json({
-          message: err.message || "Some error occurred while deleting the karyawan.",
-          data: null,
+    .then((num) => {
+      if (num == 1) {
+        res.json({
+          message: "Karyawan deleted successfully.",
+          data: req.body,
         });
+      } else {
+        res.json({
+          message: `Cannot delete karyawan with Nomor_Induk=${Nomor_Induk}. Maybe karyawan was not found!`,
+          data: req.body,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message:
+          err.message || "Some error occurred while deleting the karyawan.",
+        data: null,
       });
-  };
+    });
+};
 
-  
 //Data karyawan dan cuti
-const { QueryTypes } = require('sequelize');
-  exports.findAllC = (req, res) => {
-    const nomor_induk = req.params.nomor_induk;
-    const sql = `SELECT k.Nomor_Induk, k.Nama, k.Alamat, k.Tanggal_Lahir, k.Tanggal_Bergabung, c.* 
+const { QueryTypes } = require("sequelize");
+exports.findAllC = (req, res) => {
+  const nomor_induk = req.params.nomor_induk;
+  const sql = `SELECT k.Nomor_Induk, k.Nama, k.Alamat, k.Tanggal_Lahir, k.Tanggal_Bergabung, c.* 
                  FROM karyawan k 
                  JOIN cuti c ON k.Nomor_Induk = c.Nomor_Induk 
                  WHERE k.Nomor_Induk = '${nomor_induk}'`;
-    db.query(sql, (err, results) => {
-      if (err) throw err;
-      response(200, results, `Data Karyawan dan Cuti untuk Nomor Induk ${nomor_induk}`, res);
-    });
-  };
+  db.query(sql, (err, results) => {
+    if (err) throw err;
+    response(
+      200,
+      results,
+      `Data Karyawan dan Cuti untuk Nomor Induk ${nomor_induk}`,
+      res
+    );
+  });
+};
 
+exports.findAll = (req, res) => {
+  karyawanFromDb
+    .findAll({
+      order: [["Nama", "ASC"]], 
+    })
+    .then((karyawans) => {
+      res.json({
+        message: "Karyawans sort by name retrieved successfully.",
+        data: karyawans,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message:
+          err.message || "Some error occurred while retrieving karyawans sort by name.",
+        data: null,
+      });
+    });
+};
--- app/routes/karyawan.routes.js
+++ app/routes/karyawan.routes.js
@@ -1,12 +1,11 @@
-const karyawanController = require('../controllers/karyawan.controller');
-const router = require('express').Router();
+const karyawanController = require("../controllers/karyawan.controller");
+const router = require("express").Router();
 
-router.get('/', karyawanController.findAll);
-router.get('/cuti/:Nomor_Induk', karyawanController.findAllC);
-router.post('/', karyawanController.create);
-router.put('/:Nomor_Induk', karyawanController.update);
-router.delete('/:Nomor_Induk', karyawanController.delete);
-router.get('/sorted', karyawanController.findAll);
-  
+router.get("/", karyawanController.findAll);
+router.get("/cuti/:Nomor_Induk", karyawanController.findAllC);
+router.post("/", karyawanController.create);
+router.put("/:Nomor_Induk", karyawanController.update);
+router.delete("/:Nomor_Induk", karyawanController.delete);
+router.get("/sorted", karyawanController.findAll);
 
 module.exports = router;
